2371deed7bd43237daa8aff3e4bed3156542a612
Nandezu <info@nandezu.com>
2024-08-09 20:03:04 +0200

    Upravena konfigurace pro Render, přidány migrace

--- .history/nandeback/settings_20240809194222.py
+++ .history/nandeback/settings_20240809194222.py
@@ -1,14 +1,10 @@
 import os
 from pathlib import Path
 import dj_database_url
-from dotenv import load_dotenv
-
-# Načtení proměnných z .env souboru
-load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-h!xpc8nnawpijntcf+9+q6d4d-o)3(%e&71fx5*w=)l8#@)z2l')
+SECRET_KEY = 'django-insecure-h!xpc8nnawpijntcf+9+q6d4d-o)3(%e&71fx5*w=)l8#@)z2l'
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
@@ -77,22 +73,14 @@ WSGI_APPLICATION = 'nandeback.wsgi.application'
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True
-        )
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        conn_health_checks=True,
+        ssl_require=True
+    )
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -117,7 +105,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -149,13 +137,13 @@ LOGGING = {
     },
     'root': {
         'handlers': ['console'],
-        'level': 'INFO',
+        'level': 'DEBUG',
     },
 }
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = 'AKIA6GBMEM6GOI77EB6R'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'usersnandezu'
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_FILE_OVERWRITE = False
@@ -172,10 +160,10 @@ EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+EMAIL_HOST_PASSWORD = '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'
 DEFAULT_FROM_EMAIL = 'noreply@nandezu.com'
 
-SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
+SENDGRID_API_KEY = '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'
 
 PASSWORD_RESET_TIMEOUT = 3600
 
@@ -187,12 +175,6 @@ if os.environ.get('NANDEZU_ENV') == 'production':
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    SECURE_BROWSER_XSS_FILTER = True
-    X_FRAME_OPTIONS = 'DENY'
 else:
     EXPO_DEV_URL = 'exp://192.168.0.106:8081'
     PASSWORD_RESET_URL = f'{EXPO_DEV_URL}/--/reset-password'
@@ -204,7 +186,7 @@ WHITENOISE_USE_FINDERS = True
 WHITENOISE_MANIFEST_STRICT = False
 WHITENOISE_ALLOW_ALL_ORIGINS = True
 
-# Konfigurace pro in-app nákupy
+# Nové konfigurace pro in-app nákupy
 SUBSCRIPTION_PRODUCTS = {
     'ios': {
         'BASIC_MONTHLY': 'com.nandezu.basic_monthly',
