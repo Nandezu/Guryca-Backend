102a2d5cc1af4a4dec7078da63de86a014544bd5
Nandezu <info@nandezu.com>
2024-09-09 14:11:20 +0200

    Přidána metoda formatted_price do modelu Product a upraveny serializery

---
+++ .history/shop/serializers_20240626132746.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'store_link', 'image_url', 'sku', 'clothing_type', 'clothing_category', 'manufacturer_name', 'country_of_origin', 'colour', 'price']
